e0a64683a540a06e870d6a5ed3082e0e2c200d08
Sam Wedll <sam.wedll@gmail.com>
2015-08-15 23:55:08 +0000

    fixed typo in Users::RegistratioController(s)

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,4 @@
-class Users::RegistrationsControllers < Devise::RegistrationsController
+class Users::RegistrationsController < Devise::RegistrationsController
     
     def create
         super do |resource| 
